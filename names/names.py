44e0cdc06862a1d0861f66abe4284fcc43913c08
tlewandowski18 <tlewandowski18@gmail.com>
2020-06-28 22:11:57 -0400

    Complete reverse function

--- names/names.py
+++ names/names.py
@@ -1,5 +1,72 @@
 import time
 
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value= value
+        self.next_node = next_node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None #stores a node that corresponds to our first node in list
+        self.tail = None # stores a node that is the end of the list
+
+    #return all values om the list
+    def __str__(self):
+        output = ''
+        current_node = self.head #create tracker node variable
+
+        while current_node is not None: #loop unitl it's none
+
+            output += f'{current_node.value} -> '
+
+            current_node = current_node.next_node #update tracker node to the next node
+            
+        return output
+
+    def add_to_head(self, value):
+        #create a node to add
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
+
+    def add_to_tail(self, value):
+        #create a node to add
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+
+    def remove_head(self):
+        if not self.head:
+            return None
+        if self.head.next_node is None:
+            head_value = self.head.value
+            self.head = None
+            self.tail = None
+            return head_value
+        head_value = self.head.value
+        self.head = self.head.next_node
+        return head_value
+
+
+    def contains(self, value):
+        if self.head is None:
+            return False
+        current_node = self.head
+        while current_node is not None:
+            if current_node.value == value:
+                return True
+            current_node = current_node.next_node
+        return False
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,13 +77,16 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+names_2_ll = LinkedList()
+for name in names_2:
+    names_2_ll.add_to_head(name)
+
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if names_2_ll.contains(name_1):
+        duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
